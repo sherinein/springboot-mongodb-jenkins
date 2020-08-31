a546e33fd204b829ea55dabb97af055c951a5e95
sherine <sherinein@gmail.com>
2020-08-31 08:22:32 +0530

    Logger add in service layer in add employee

--- src/main/java/org/dxc/mongodb/service/EmployeeServiceImpl.java
+++ src/main/java/org/dxc/mongodb/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package org.dxc.mongodb.service;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 import org.dxc.mongodb.model.Employee;
 import org.dxc.mongodb.repository.EmployeeRepository;
@@ -13,18 +14,22 @@ import org.springframework.stereotype.Service;
 public class EmployeeServiceImpl implements EmployeeService{
 	@Autowired
 	private EmployeeRepository employeeRepository;
+	private Logger logger = Logger.getLogger(getClass().getName());
 	@Override
 	public void saveEmployee(List<Employee> employee) {
-		
+		logger.info("==========saveEmployee -EmployeeServiceImpl ===========");
 		 employeeRepository.saveAll(employee);                          //    [ { "empid":101,"empname":"sherine" },{ "empid":102,"empname":"ram" },{ "empid":103,"empname":"arun" } ]  , { "empid":101,"empname":"sherine"}
 	}
 	@Override
 	public Collection<Employee> getAllEmployees() {
+	
 		return employeeRepository.findAll();
 	}
 	@Override
 	public Optional<Employee> findEmployeeById(int empid) {
+		
 		return employeeRepository.findById(empid);
+		
 	}
 	@Override
 	public void deleteEmployeeById(int empid) {
